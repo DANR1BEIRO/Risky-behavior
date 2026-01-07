94a4f157b86d1823fbf30fc80a0853f6c5a33397
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-07 10:54:18 -0300

    refactor: Troca as inner classes por lambdas

--- src/GUI/TwoButtons.java
+++ src/GUI/TwoButtons.java
@@ -10,7 +10,38 @@ public class TwoButtons {
     private JLabel label;
     private MyDrawPanel panel;
 
-    class LabelListener implements ActionListener {
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        panel = new MyDrawPanel();
+
+        JButton labelButton = new JButton("Change Label");
+        labelButton.setFont(new Font("Hack", Font.BOLD, 20));
+        labelButton.addActionListener(event -> label.setText("Text changed"));
+
+        JButton colorButton = new JButton("Change Color");
+        colorButton.setFont(new Font("Hack", Font.BOLD, 80));
+        colorButton.addActionListener(event -> panel.changeColor());
+
+        label = new JLabel("I'm a label");
+        label.setFont(new Font("Hack", Font.BOLD, 20));
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+        frame.setSize(1700, 1700);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TwoButtons gui = new TwoButtons();
+        gui.go();
+    }
+}
+
+    /*class LabelListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
             label.setText("Text chenged");
@@ -22,35 +53,4 @@ public class TwoButtons {
         public void actionPerformed(ActionEvent actionEvent) {
             panel.changeColor();
         }
-    }
-
-    public static void main(String[] args) {
-        TwoButtons gui = new TwoButtons();
-        gui.go();
-    }
-
-    public void go() {
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        panel = new MyDrawPanel();
-
-        JButton labelButton = new JButton("Change Label");
-        labelButton.setFont(new Font("Hack", Font.BOLD, 20));
-        labelButton.addActionListener(new LabelListener());
-
-        JButton colorButton = new JButton("Change Color");
-        colorButton.setFont(new Font("Hack", Font.BOLD, 80));
-        colorButton.addActionListener(new ColorListener());
-
-        label = new JLabel("I'm a label");
-        label.setFont(new Font("Hack", Font.BOLD, 20));
-
-
-        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
-        frame.getContentPane().add(BorderLayout.EAST, labelButton);
-        frame.getContentPane().add(BorderLayout.WEST, label);
-        frame.setSize(1700, 1700);
-        frame.setVisible(true);
-    }
-}
+    }*/
