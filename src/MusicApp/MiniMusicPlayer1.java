a997e70f564bbe29294f556d1cd4a5665784db96
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-08 10:38:36 -0300

    feat: O mini app agora gera retangulos com tamanhos e cores aleat orios a cada batida do instrumento

--- src/MusicApp/MiniMusicPlayer1.java
+++ src/MusicApp/MiniMusicPlayer1.java
@@ -1,33 +1,45 @@
 package MusicApp;
 
+import GUI.MyDrawPanel;
+
 import javax.sound.midi.*;
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
 
 import static javax.sound.midi.ShortMessage.*;
 
 public class MiniMusicPlayer1 {
+    private MyDrawPanel panel;
+    private Random random = new Random();
+
     public static void main(String[] args) {
         MiniMusicPlayer1 player = new MiniMusicPlayer1();
         player.go();
     }
 
+    public void setUpGui() {
+        JFrame frame = new JFrame("My first music video");
+        panel = new MyDrawPanel();
+        frame.setContentPane(panel);
+        frame.setBounds(90, 90, 1200, 1200);
+        frame.setVisible(true);
+    }
+
     public void go() {
+        setUpGui();
+
         try {
             Sequencer sequencer = MidiSystem.getSequencer(); // make and open a sequencer
             sequencer.open();
-
-            // Register for events with the sequencer.
-            // The event registration method takes the listener AND an int array representing
-            // the list of ControllerEvents I want. We want care about one event, #127
-            int[] eventsIWant = {127};
-            sequencer.addControllerEventListener(event -> System.out.println("la"), eventsIWant);
-            // Each time we get the event, we'll print "la" to the command line.
-
-            // make a sequence and track
+            sequencer.addControllerEventListener(panel, new int[]{127});
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
-            // Make a bunch of events to make the notes keep going up (from piano note 5 to piano note 61)
+            int note;
             for (int i = 0; i < 61; i += 4) {
+                note = random.nextInt(50) + 1;
                 track.add(makeEvent(NOTE_ON, 1, i, 100, i));
                 track.add(makeEvent(CONTROL_CHANGE, 1, 127, 0, i));
                 track.add(makeEvent(NOTE_OFF, 1, i, 100, i + 2));
@@ -52,6 +64,34 @@ public class MiniMusicPlayer1 {
             e.printStackTrace();
         }
         return event;
+    }
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+        private boolean msg = false;
+
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+
+        public void paintComponent(Graphics g) {
+            if (msg) {
+                int red = random.nextInt(256);
+                int green = random.nextInt(256);
+                int blue = random.nextInt(256);
+
+                g.setColor(new Color(red, green, blue));
+
+                int height = random.nextInt(120) + 10;
+                int width = random.nextInt(120) + 10;
+
+                int xPos = random.nextInt(40) + 10;
+                int yPos = random.nextInt(40) + 10;
+
+                g.fillRect(xPos, yPos, width, height);
+                msg = false;
+            }
+        }
     }
 }
 
