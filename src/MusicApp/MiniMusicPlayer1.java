c6e99895451218c93134025f2f327f19921d749a
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-08 10:22:16 -0300

    feat: Implementa listener para eventos de controle MIDI
    - move a lógica de negócio para um método go()
    - adiciona um ControllerEventListener para monitorar o evento #127
    - Utiliza uma expressão Lambda para imprimir "la" no console em tempo real.
    - Insere o evento CONTROL_CHANGE na track para servir como gatilho (trigger).

--- src/MusicApp/MiniMusicPlayer1.java
+++ src/MusicApp/MiniMusicPlayer1.java
@@ -2,14 +2,25 @@ package MusicApp;
 
 import javax.sound.midi.*;
 
-import static javax.sound.midi.ShortMessage.NOTE_OFF;
-import static javax.sound.midi.ShortMessage.NOTE_ON;
+import static javax.sound.midi.ShortMessage.*;
 
 public class MiniMusicPlayer1 {
     public static void main(String[] args) {
+        MiniMusicPlayer1 player = new MiniMusicPlayer1();
+        player.go();
+    }
+
+    public void go() {
         try {
             Sequencer sequencer = MidiSystem.getSequencer(); // make and open a sequencer
             sequencer.open();
+
+            // Register for events with the sequencer.
+            // The event registration method takes the listener AND an int array representing
+            // the list of ControllerEvents I want. We want care about one event, #127
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(event -> System.out.println("la"), eventsIWant);
+            // Each time we get the event, we'll print "la" to the command line.
 
             // make a sequence and track
             Sequence seq = new Sequence(Sequence.PPQ, 4);
@@ -18,6 +29,7 @@ public class MiniMusicPlayer1 {
             // Make a bunch of events to make the notes keep going up (from piano note 5 to piano note 61)
             for (int i = 0; i < 61; i += 4) {
                 track.add(makeEvent(NOTE_ON, 1, i, 100, i));
+                track.add(makeEvent(CONTROL_CHANGE, 1, 127, 0, i));
                 track.add(makeEvent(NOTE_OFF, 1, i, 100, i + 2));
             }
 
@@ -28,6 +40,7 @@ public class MiniMusicPlayer1 {
             e.printStackTrace();
         }
     }
+
 
     public static MidiEvent makeEvent(int command, int channel, int one, int two, int tick) {
         MidiEvent event = null;
@@ -41,3 +54,4 @@ public class MiniMusicPlayer1 {
         return event;
     }
 }
+
